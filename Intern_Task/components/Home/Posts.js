69dba93906ba1b422d5f35ec43ad5af939957da1
Vedantttgitteee20 <you@example.com>
2023-09-11 16:15:19 +0530

    strip added

--- Intern_Task/components/Home/Posts.js
+++ Intern_Task/components/Home/Posts.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, ImageBackground, StyleSheet, TouchableOpacity } from 'react-native';
 import { Asset } from 'expo-asset';
+import { AntDesign, Entypo, Ionicons, FontAwesome } from '@expo/vector-icons';
 const postsData = [
   {
     id: '1',
@@ -8,6 +9,8 @@ const postsData = [
     email: '@balancha20',
     imageUrl: Asset.fromModule(require('../../assets/stories/vedant.jpeg')).uri,
     caption: 'A beautiful sunset view. 🌅',
+    likes: '12k',
+    options:'12',
   },
   {
     id: '2',
@@ -15,6 +18,8 @@ const postsData = [
     email: '@balancha20',
     imageUrl: Asset.fromModule(require('../../assets/stories/dummy.jpeg')).uri,
     caption: 'Exploring new places! 🌍',
+    likes: '24k',
+    options:'14',
   },
   // Add more posts here
 ];
@@ -31,8 +36,28 @@ const Posts = () => {
                     <Text style={styles.email}>{post.email}</Text>
                 </View>
             </View>
-          <Image source={{ uri: post.imageUrl }} style={styles.postImage} />
-          {/* <Text style={styles.caption}>{post.caption}</Text> */}
+          <ImageBackground source={{ uri: post.imageUrl }} style={styles.postImage}>
+              <View style={styles.overlayStrip}>
+                <View style={styles.box1}>
+                  <TouchableOpacity style={styles.optionButton}>
+                    <Entypo name="dots-three-horizontal" size={24} color="#DADADA" />
+                    <Text style={styles.optionButtonText}>{post.options}</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.optionButton}>
+                    <AntDesign name="heart" size={30} color="#DADADA" />
+                    <Text style={styles.likeButtonText}>{post.likes}</Text>
+                  </TouchableOpacity>
+                </View>
+                <View style={styles.box1}>
+                  <TouchableOpacity style={styles.optionButton}>
+                    <Ionicons name="ios-paper-plane" size={30} color="#DADADA" />
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.optionButton}>
+                    <FontAwesome name="bookmark" size={30} color="#DADADA" />
+                  </TouchableOpacity>
+                </View>
+              </View>
+          </ImageBackground>
         </View>
       ))}
     </View>
@@ -51,6 +76,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#BDD5E7',
     padding: 5,
     borderRadius: 40,
+    
   },
   username: {
     fontWeight: 'bold',
@@ -80,9 +106,51 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 270,
     resizeMode: 'cover',
-    borderRadius: 40,
+    borderRadius: 30,
     marginTop: 8,
     marginBottom: 8,
+    overflow: 'hidden',
+  },
+  overlayStrip: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.7)', // Adjust the opacity and color as needed
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 10,
+  },
+  box1:{
+    flexDirection: 'row',
+  },
+  optionButton: {
+    flexDirection:'row',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+  optionButtonText: {
+    color: "#DADADA",
+    //fontFamily: 'Poppins', // Use the name of your imported font
+    fontWeight: '600', // This should be a string, not a number
+    fontSize: 13,
+    lineHeight: 22,
+    marginLeft:5,
+  },
+  likeButtonText: {
+    color: "#DADADA",
+    //fontFamily: 'Poppins', // Use the name of your imported font
+    fontWeight: '600', // This should be a string, not a number
+    fontSize: 13,
+    lineHeight: 22,
+    marginLeft:5,
+  },
+  button: {
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 5,
   },
   caption: {
     fontSize: 14,
